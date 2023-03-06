5422450b4ff77f340b5944a2113dd5d2ec585cb1
Andrew <AndrewMelad>
2023-03-06 21:22:44 +0200

    start design login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,19 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import loginPage from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "login",
+    component: loginPage,
   },
   {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,26 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "login-page-image": "url('assets/pexels-todd-trapani-1777832.jpg')",
+      },
+      colors: {
+        "black-color": "rgba(2,2,2,0.4)",
+      },
+      // keyframes: {
+      //   "up-down": {
+      //     "0%": { transform: "translateY(-25%)" },
+
+      //     "50%": { transform: "translateY(0)" },
+      //     " 75%": { transform: "translateY(-25%)" },
+      //     " 100%": { transform: "translateY(0)" },
+      //   },
+      // },
+      animation: {
+        "up-down": "bounce 1s ease-in-out ",
+      },
+    },
   },
   plugins: [],
 };
